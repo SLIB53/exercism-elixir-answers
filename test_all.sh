6c3fa7ab96789b6c52e7fbf4921b9c64eb3eaf32
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-10-24 13:20:30 -0500

    Add Travis CI integration

---
+++ test_all.sh
@@ -0,0 +1,12 @@
+#!/bin/env sh
+
+# Runs tests for each exercise, exiting with 1 if a test fails. Tests are found
+# by convention, where the test is an elixir script ending with "_test.exs".
+
+for item in $(find . -name "*_test.exs")
+do
+    if ! elixir $item; then
+        echo "A test in $item failed."
+        exit 1
+    fi
+done
