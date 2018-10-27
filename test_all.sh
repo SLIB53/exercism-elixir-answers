3e0cbca3fec7fce9b2aef2a0657253bf943757de
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-10-26 22:38:39 -0500

    Update test_all.sh, Run tests alphabetically

--- test_all.sh
+++ test_all.sh
@@ -3,7 +3,7 @@
 # Runs tests for each exercise, exiting with 1 if a test fails. Tests are found
 # by convention, where the test is an elixir script ending with "_test.exs".
 
-for item in $(find . -name "*_test.exs")
+for item in $(find . -name "*_test.exs" | sort)
 do
     if ! elixir $item; then
         echo "A test in $item failed."
